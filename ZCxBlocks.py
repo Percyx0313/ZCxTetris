9e78e3a057fe072d315f23387c65603ea9ee147c
Percyx0313 <percyx987654321@gmail.com>
2021-07-10 01:32:03 +0800

    add the rotate direction

--- ZCxBlocks.py
+++ ZCxBlocks.py
@@ -11,16 +11,27 @@
 #  x       x *      * x        x *
 #  * * 
 #================================================
-#               J,L,S,T,Z Offset 
-#             
-#
-#
-#
-#
-#
-#
-#               I Offset 
-#
+#           rotate type : O->R->2->L
+#               J,L,S,T,Z Wall_Kick 
+#       test1   test2   test3   test4   test5
+#   O   (0,0)   (0,0)   (0,0)   (0,0)   (0,0)
+#   R   (0,0)   (+1,0)  (+1,-1) (0,+2)  (+1,+2)
+#   2   (0,0)   (0,0)   (0,0)   (0,0)   (0,0)
+#   L   (0,0)   (-1,0)  (-1,-1) (0,+2)  (-1,+2)
+#================================================
+#                   I Wall_Kick
+#       test1   test2   test3   test4   test5
+#   O   (0,0)   (-1,0)  (+2,0)  (-1,0)  (+2,0)
+#   R   (-1,0)  (0,0)   (0,0)   (0,+1)  (0,-2)
+#   2   (-1,+1) (+1,+1) (-2,+1) (+1,0)  (-2,0)
+#   L   (0,+1)  (0,+1)  (0,+1)  (0,-1)  (0,+2)
+#================================================
+#                   O Wall_Kick
+#       test1   test2   test3   test4   test5
+#   O   (0,0)
+#   R   (0,-1)
+#   2   (-1,-1)
+#   L   (-1,0)
 #================================================
 #This class only has attribution Blocks
 class ZCxBlock:
@@ -80,30 +91,42 @@ class ZCxBlock:
                     [0,0,0,0]]
         self.BlockType='Z'
         self.Rotate_Type=3
-    def R_Rotate(self):
+    def Rotate(self,LorR):
         temp=[]
-        for i in range(len(self.Block)):
-            temp.append(self.Block[i].copy())
-        if self.Rotate_Type==3:
-            for y in range(3):
-                for x in range(3):
-                    index=6-3*x+y
-                    self.Block[y][x]=temp[index//3][index%3]
+        if LorR=="R":
+            for i in range(len(self.Block)):
+                temp.append(self.Block[i].copy())
+            if self.Rotate_Type==3:
+                for y in range(3):
+                    for x in range(3):
+                        index=6-3*x+y
+                        self.Block[y][x]=temp[index//3][index%3]
         
-        elif self.Rotate_Type==4:
-            for y in range(4):
-                for x in range(4):
-                    index=12-4*x+y
-                    self.Block[y][x]=temp[index//4][index%4]
+            elif self.Rotate_Type==4:
+                for y in range(4):
+                    for x in range(4):
+                        index=12-4*x+y
+                        self.Block[y][x]=temp[index//4][index%4]
+        elif LorR=="L":
+            for i in range(len(self.Block)):
+                temp.append(self.Block[i].copy())
+            if self.Rotate_Type==3:
+                for y in range(3):
+                    for x in range(3):
+                        index=3*x+2 -y
+                        self.Block[y][x]=temp[index//3][index%3]
+        
+            elif self.Rotate_Type==4:
+                for y in range(4):
+                    for x in range(4):
+                        index=4*x+3-y
+                        self.Block[y][x]=temp[index//4][index%4]
 
     def show_block(self):
         for y in range(4):
             print(self.Block[y][0],self.Block[y][1],self.Block[y][2],self.Block[y][3])
        
-test_block=ZCxBlock()
-test_block.SetType_J()
-test_block.R_Rotate()
-test_block.show_block()
+
 
 
 
